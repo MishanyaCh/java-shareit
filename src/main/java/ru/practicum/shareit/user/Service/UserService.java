615ab9cf9007896365a0a7a833cfefe034d13861
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-02-05 18:05:10 +0300

    style: удалил невидимые пробелы

--- src/main/java/ru/practicum/shareit/user/Service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/Service/UserService.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 public interface UserService {
     UserDto createUser(UserDto userDto);
-    
+
     UserDto updateUser(int id, UserDto userDto);
 
     List<UserDto> getUsers();
-
+    
     UserDto getUser(int id);
 
     void deleteUser(int id);
