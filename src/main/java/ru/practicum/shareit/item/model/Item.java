3bf6aded823ecb7676659ad0d807561bd6271b0d
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-02-05 15:21:21 +0300

    feat: класс Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,36 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Item {
+    private int id;
+    private String name;
+    private String description;
+    private Boolean isAvailable; // статус: если true, то доступна для аренды, а если false - недоступна
+    private int ownerId; // владелец вещи
+
+    public Item(int idArg, String nameArg, String descriptionArg, Boolean isAvailableArg, int ownerIdArg) {
+        id = idArg;
+        name = nameArg;
+        description = descriptionArg;
+        isAvailable = isAvailableArg;
+        ownerId = ownerIdArg;
+    }
+
+    @Override
+    public String toString() {
+        String result = "Item{" + "id=" + id + ", name='" + name + '\'';
+
+        if (description != null) {
+            result = result + ", description.length=" + description.length();
+        } else {
+            result = result + ", description.length='null'";
+        }
+        return result + ", isAvailable=" + isAvailable + ", ownerId=" + ownerId + "}" + '\n';
+    }
 }
