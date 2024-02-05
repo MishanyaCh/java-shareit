10004259e2ca6abae9b97a13bafdb98d11443e0e
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-02-05 18:10:22 +0300

    fix: устраняем баг

--- src/main/java/ru/practicum/shareit/user/Service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/Service/UserService.java
@@ -5,13 +5,4 @@ import ru.practicum.shareit.user.dto.UserDto;
 import java.util.List;
 
 public interface UserService {
-    UserDto createUser(UserDto userDto);
-
-    UserDto updateUser(int id, UserDto userDto);
-
-    List<UserDto> getUsers();
-    
-    UserDto getUser(int id);
-
-    void deleteUser(int id);
 }
--- src/main/java/ru/practicum/shareit/user/Storage/UserStorage.java
+++ src/main/java/ru/practicum/shareit/user/Storage/UserStorage.java
@@ -5,13 +5,4 @@ import ru.practicum.shareit.user.model.User;
 import java.util.List;
 
 public interface UserStorage {
-    User createUser(User user);
-
-    User updateUser(User user);
-    
-    List<User> getUsers();
-
-    User getUser(int id);
-
-    void deleteUser(int id);
 }
