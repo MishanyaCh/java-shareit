6de184a82687cf0c82b02beed7c60d0a54c1c3a0
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-02-05 18:19:29 +0300

    fix: написал заново интерфейс UserStorage

--- src/main/java/ru/practicum/shareit/user/Storage/UserStorage.java
+++ src/main/java/ru/practicum/shareit/user/Storage/UserStorage.java
@@ -5,4 +5,13 @@ import ru.practicum.shareit.user.model.User;
 import java.util.List;
 
 public interface UserStorage {
+    User createUser(User user);
+
+    User updateUser(User user);
+
+    List<User> getUsers();
+
+    User getUser(int id);
+
+    void deleteUser(int id);
 }
