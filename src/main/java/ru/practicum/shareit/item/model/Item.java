0bf18aa6f518189d8eaa2f3aaf28ba5c034a52c2
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-03-08 15:23:53 +0300

    reafact: откорректировал класс Item для работы с hibernate

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,18 +1,39 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Getter
 @Setter
-@EqualsAndHashCode
+@Entity
+@Table(name = "items")
 public class Item {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "is_available")
     private Boolean isAvailable; // статус: если true, то доступна для аренды, а если false - недоступна
+
+    @Column(name = "owner_id")
     private int ownerId; // владелец вещи
+
+    public Item() { // конструктор без параметров для работы hibernate
+    }
 
     public Item(int idArg, String nameArg, String descriptionArg, Boolean isAvailableArg, int ownerIdArg) {
         id = idArg;
