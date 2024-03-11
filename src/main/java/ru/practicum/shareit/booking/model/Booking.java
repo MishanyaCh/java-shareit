d6851beaea478851c3daceb8833fa9f3242cd4ea
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-03-11 23:16:57 +0300

    feat: класс booking

---
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -0,0 +1,54 @@
+package ru.practicum.shareit.booking.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import ru.practicum.shareit.booking.enums.Status;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * TODO Sprint add-bookings.
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = "bookings")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item; // вещь, бронируемая пользователем
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booker_id")
+    private User booker;// пользователь, бронирующий вещь
+
+    @Column(name = "start_booking_data")
+    private LocalDateTime startBookingData; // начало бронирования
+
+    @Column(name = "end_booking_data")
+    private LocalDateTime endBookingData; // окончание бронирования
+
+    @Column(name = "booking_status")
+    @Enumerated(EnumType.STRING)
+    private Status status; // текущий статус бронирования
+
+    public Booking() { // конструктор без параметров для работы hibernate
+    }
+
+    public Booking(int idArg, Item itemArg, User bookerArg, LocalDateTime startBookingDataArg,
+                   LocalDateTime endBookingDataArg, Status statusArg) {
+        id = idArg;
+        item = itemArg;
+        booker = bookerArg;
+        startBookingData = startBookingDataArg;
+        endBookingData = endBookingDataArg;
+        status = statusArg;
+    }
+}
