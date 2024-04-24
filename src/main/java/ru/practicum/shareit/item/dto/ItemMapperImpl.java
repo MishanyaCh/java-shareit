4359d81818189c57f9058b48c20593664d981da8
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-04-24 22:20:13 +0300

    feat: в класс ItemMapperImpl добавлена реализация метода toItemDtoWithBookingAndComment

--- src/main/java/ru/practicum/shareit/item/dto/ItemMapperImpl.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapperImpl.java
@@ -1,6 +1,8 @@
 package ru.practicum.shareit.item.dto;
 
 import org.springframework.stereotype.Component;
+import ru.practicum.shareit.booking.dto.BookingDtoWithoutItemField;
+import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.ArrayList;
@@ -11,7 +13,7 @@ public class ItemMapperImpl implements ItemMapper {
 
     @Override
     public ItemDto toItemDto(Item item) {
-        return new ItemDto(item.getId(), item.getName(), item.getDescription(), item.getIsAvailable(), item.getOwnerId());
+        return new ItemDto(item.getId(), item.getName(), item.getDescription(), item.getIsAvailable());
     }
 
     @Override
@@ -28,5 +30,25 @@ public class ItemMapperImpl implements ItemMapper {
             dtoList.add(itemDto);
         }
         return dtoList;
+    }
+
+    @Override
+    public ItemDtoWithBookingAndComment toItemDtoWithBookingAndComment(Item item,
+                                                                       Booking lastBooking,
+                                                                       Booking nextBooking,
+                                                                       List<CommentDto> comments) {
+        BookingDtoWithoutItemField last = null;
+        BookingDtoWithoutItemField next = null;
+
+        if (lastBooking != null) {
+            last = new BookingDtoWithoutItemField(lastBooking.getId(), lastBooking.getBooker().getId(),
+                    lastBooking.getStartBookingDate(), lastBooking.getEndBookingDate());
+        }
+        if (lastBooking != null) {
+            next = new BookingDtoWithoutItemField(nextBooking.getId(), nextBooking.getBooker().getId(),
+                    nextBooking.getStartBookingDate(), nextBooking.getEndBookingDate());
+        }
+        return new ItemDtoWithBookingAndComment(item.getId(), item.getName(), item.getDescription(),
+                item.getIsAvailable(), last, next, comments);
     }
 }
