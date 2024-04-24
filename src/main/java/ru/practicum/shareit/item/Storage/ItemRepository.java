0f14b18a9bc179932fd5a4cd465b51e9a79c561d
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-04-24 22:32:28 +0300

    feat: добавил запросные методы в ItemRepository

--- src/main/java/ru/practicum/shareit/item/Storage/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/Storage/ItemRepository.java
@@ -7,9 +7,10 @@ import ru.practicum.shareit.item.model.Item;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
-    @Query(value = "SELECT * " +
-            "FROM items " +
-            "WHERE (LOWER(name) LIKE '%:text%' OR LOWER(description) LIKE '%:text%') AND is_available = true",
+    @Query(value = "SELECT * FROM items " +
+            "WHERE (LOWER(name) LIKE %:text% OR LOWER(description) LIKE %:text%) AND is_available = true",
             nativeQuery = true)
     List<Item> searchItems(String text);
+
+    List<Item> findItemsByOwnerId(int ownerId);
 }
