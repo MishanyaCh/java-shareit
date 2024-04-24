4031e86d4c044d364475ea21a4feb1577756cd8d
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-04-24 23:52:36 +0300

    style: убрал неиспользующийся импорт в классе ItemDto

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
