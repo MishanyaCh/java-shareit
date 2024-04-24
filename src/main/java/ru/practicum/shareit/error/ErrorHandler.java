9c5e32a02f3fe3c41300988a392df6240484b5e5
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-04-24 21:50:19 +0300

    feat: добавил обработку ошибок для класса BookingServiceImpl

--- src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -6,13 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.shareit.booking.BookingController;
 import ru.practicum.shareit.exception.*;
 import ru.practicum.shareit.item.ItemController;
 import ru.practicum.shareit.user.UserController;
 
 import java.util.Map;
 
-@RestControllerAdvice(assignableTypes = {ItemController.class, UserController.class})
+@RestControllerAdvice(assignableTypes = {ItemController.class, UserController.class, BookingController.class})
 public class ErrorHandler {
     private static final Logger log = LoggerFactory.getLogger(ErrorHandler.class);
 
@@ -42,5 +43,40 @@ public class ErrorHandler {
     public Map<String, String> handleHeaderNonexistentException(final HeaderNonexistentException exc) {
         log.error('\n' + "Отловлена ошибка: " + exc.getMessage() + '\n');
         return Map.of("Error", exc.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleObjectNotAvailableException(final ObjectNotAvailableException exc) {
+        log.error('\n' + "Отловлена ошибка: " + exc.getMessage() + '\n');
+        return new ErrorResponse(exc.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleDateTimeBookingException(final DateTimeBookingException exc) {
+        log.error('\n' + "Отловлена ошибка: " + exc.getMessage() + '\n');
+        return new ErrorResponse(exc.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleUnknownStateException(final UnknownStateException exc) {
+        log.error('\n' + "Отловлена ошибка: " + exc.getMessage() + '\n');
+        return new ErrorResponse(exc.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public ErrorResponse BookingException(final BookingException exc) {
+        log.error('\n' + "Отловлена ошибка: " + exc.getMessage() + '\n');
+        return new ErrorResponse(exc.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBookingIsApprovedException(final BookingIsApprovedException exc) {
+        log.error('\n' + "Отловлена ошибка: " + exc.getMessage() + '\n');
+        return new ErrorResponse(exc.getMessage());
     }
 }
