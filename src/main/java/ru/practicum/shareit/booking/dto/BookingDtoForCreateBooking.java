42ed21bc89e8f033403b6ff8c752c685a5d80077
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-04-24 21:23:27 +0300

    feat: класс bookingDtoForCreatingBooking

---
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDtoForCreateBooking.java
@@ -0,0 +1,35 @@
+package ru.practicum.shareit.booking.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class BookingDtoForCreateBooking {
+    @NotNull(message = "Поле с идентификатором бронируемой вещи должно присутствовать!")
+    private Integer itemId;
+
+    @NotNull(message = "Поле c датой начала бронирования должно присутствовать!")
+    @FutureOrPresent(message = "Дата начала бронирования не может быть раньше чем текущий день")
+    private LocalDateTime start;
+
+    @NotNull(message = "Поле с датой окончания бронирования должно присутствовать!")
+    @Future(message = "Дата окончания бронирования не может быть прошедшим днем!")
+    private LocalDateTime end;
+
+    public BookingDtoForCreateBooking(Integer itemIdArg, LocalDateTime startArg, LocalDateTime endArg) {
+        itemId = itemIdArg;
+        start = startArg;
+        end = endArg;
+    }
+
+    @Override
+    public String toString() {
+        return "BookingDtoForCreateBooking{" + "itemId=" + itemId + ", start=" + start + ", end=" + end + "}" + '\n';
+    }
+}
