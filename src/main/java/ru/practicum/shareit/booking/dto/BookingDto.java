50e32d0f4359bf5048b3f7919844bb648f632845
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-04-24 21:21:49 +0300

    feat: класс bookingDto

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,55 @@
 package ru.practicum.shareit.booking.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint add-bookings.
  */
+@Getter
+@Setter
 public class BookingDto {
+    private int id;
+
+    @JsonProperty("start")
+    private LocalDateTime startBookingDate;
+
+    @JsonProperty("end")
+    private LocalDateTime endBookingDate;
+    private String status;
+
+    @JsonProperty("booker")
+    private UserDto bookerDto;
+
+    @JsonProperty("item")
+    private ItemDto itemDto;
+
+    public BookingDto(int idArg, LocalDateTime startBookingDateArg, LocalDateTime endBookingDateArg,
+                      String statusArg, UserDto bookerArg, ItemDto ItemArg) {
+        id = idArg;
+        startBookingDate = startBookingDateArg;
+        endBookingDate = endBookingDateArg;
+        status = statusArg;
+        bookerDto = bookerArg;
+        itemDto = ItemArg;
+    }
+
+    @Override
+    public String toString() {
+        String result = "BookingDto{" + "id=" + id + ", startBookingDate=" + startBookingDate +
+                ", endBookingDate=" + endBookingDate + ", status='" + status + '\'';
+
+        if (bookerDto != null) {
+            result = result + ", " + bookerDto.toString();
+        }
+        if (itemDto != null) {
+            result = result + ", " + itemDto.toString();
+        }
+        return result + "}" + '\n';
+    }
 }
