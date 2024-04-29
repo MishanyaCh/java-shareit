a5d95d0cf1744072dafe54469908eb56c4e3e390
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-04-29 18:45:46 +0300

    delete: в классе BookingController удалил комментарий TODO

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -21,9 +21,6 @@ import ru.practicum.shareit.booking.dto.BookingDtoForCreateBooking;
 import javax.validation.Valid;
 import java.util.List;
 
-/**
- * TODO Sprint add-bookings.
- */
 @RestController
 @RequestMapping(path = "/bookings")
 public class BookingController {
