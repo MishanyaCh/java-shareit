044c5c5ffc75343a38d1aaeb3f6dca3f935cae8f
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-06-01 10:29:11 +0300

    feat: класс ItemRequestDto

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,7 +1,28 @@
 package ru.practicum.shareit.request.dto;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class ItemRequestDto {
+    private Integer id;
+
+    @NotBlank(message = "Описание желаемой вещи должно содержать хотя бы один непробельный символ!")
+    private String description;
+    private LocalDateTime created;
+
+    public ItemRequestDto(Integer idArg, String descriptionArg, LocalDateTime createdArg) {
+        id = idArg;
+        description = descriptionArg;
+        created = createdArg;
+    }
+
+    public String toString() {
+        return "ItemRequestDto{" + "id=" + id + ", description='" + description + '\'' + ", created=" + created + "}";
+    }
 }
