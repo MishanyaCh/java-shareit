d0453533b73a7f6e11db4d109f238265f15a849d
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-06-03 04:57:45 +0300

    fix: для поля id в классе ItemDto заменил тип int на Integer

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 @Setter
 public class ItemDto {
-    private int id;
+    private Integer id;
 
     @NotBlank
     private String name;
@@ -25,18 +25,20 @@ public class ItemDto {
 
     @JsonIgnore
     private int ownerId; // владелец вещи
+    private Integer requestId; // если вещь добавляется по запросу, то переменная request != null
 
-    public ItemDto(int idArg, String nameArg, String descriptionArg, Boolean isAvailableArg) {
+    public ItemDto(Integer idArg, String nameArg, String descriptionArg, Boolean isAvailableArg, Integer requestIdArg) {
         id = idArg;
         name = nameArg;
         description = descriptionArg;
         isAvailable = isAvailableArg;
         ownerId = 0;
+        requestId = requestIdArg;
     }
 
     @Override
     public String toString() {
         return "ItemDto{" + "id=" + id + ", name='" + name + '\'' + ", description='" + description + '\'' +
-                ", isAvailable=" + isAvailable + "}" + '\n';
+                ", isAvailable=" + isAvailable +  ", requestId=" + requestId + "}" + '\n';
     }
 }
